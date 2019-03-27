3472cf72f84fda348ac61d2d91e7f1f073288328
Arunkumar L <arunk609@gmail.com>
2019-03-28 01:01:28 +0530

    Project Completion

---
+++ main.py
@@ -0,0 +1,46 @@
+import numpy as np
+import os
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import math
+
+
+generator = load_model('dcgan_mnist.h5')
+
+
+
+def plot_images(generator,
+                noise_input,
+                show=False,
+                step=0,
+                model_name="gan"):
+
+    os.makedirs(model_name, exist_ok=True)
+    filename = os.path.join(model_name, "%05d.png" % step)
+    images = generator.predict(noise_input)
+    plt.figure(figsize=(2.2, 2.2))
+    num_images = images.shape[0]
+    image_size = images.shape[1]
+    rows = int(math.sqrt(noise_input.shape[0]))
+    for i in range(num_images):
+        plt.subplot(rows, rows, i + 1)
+        image = np.reshape(images[i], [image_size, image_size])
+        plt.imshow(image, cmap='gray')
+        plt.axis('off')
+    plt.savefig(filename)
+    if show:
+        plt.show()
+    else:
+        plt.close('all')
+
+
+def test_generator(generator):
+    noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
+    plot_images(generator,
+                noise_input=noise_input,
+                show=True,
+                model_name="test_outputs")
+
+
+if __name__ == '__main__':
+    test_generator(generator)
